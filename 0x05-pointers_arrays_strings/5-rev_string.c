56fed82cf8c444a6f73aa2233d9c62eea126ed17
Adex24ty <orebanwoadewale@gmail.com>
2022-03-21 21:22:22 +0000

    New C project

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * rev_string - reverse string
- *   * @s: string
- *    * Return: void
- *     */
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ */
 
 void rev_string(char *s)
 {
